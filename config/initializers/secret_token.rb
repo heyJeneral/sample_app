70f70e19baf6198dfe99bdb677505d6ba139c5f7
Jennifer Thomas <jennifer.thomas@travel720.com>
2012-04-30 12:41:24 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '82d143ad3692c99e780388cad563458833834e31f42609237640bb360b9122e2cba3d2f1d4a2bb9a0361998d2a1c145930d941ac2a5980971ea55bb26a7362b2'
